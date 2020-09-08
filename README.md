27cd9e33ad7a7e2feedd66331b349d99569507c6
Karl Horky <karl.horky@gmail.com>
2020-09-08 11:14:08 +0200

    Add first working version of program

---
+++ README.md
@@ -0,0 +1,12 @@
+# Emoji Picker
+
+Shows the emoji corresponding to the argument
+
+```sh
+$ node index.js grinning
+😀
+$ node index.js cat
+🐱
+$ node index.js non-existent
+Emoji not found
+```
--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('');
+const emoji = require('emojilib');
+
+const emojiLibrary = emoji.lib;
+const command = process.argv[2];
+
+// If the command exists in the library
+if (emojiLibrary[command]) {
+  console.log(emojiLibrary[command].char);
+} else {
+  console.log('Emoji not found');
+}
